f021846604e3f85b52d566f497091e5a93c911c4
JieWHuang <848402168@qq.com>
2018-01-24 20:20:01 +0800

    zhihu_crawler_test

---
+++ new_zhihu_topic_crawler.py
@@ -0,0 +1,74 @@
+import json
+import new_zhihu_common
+from new_zhihu_common import mydb
+import time
+
+start_url = 'https://www.zhihu.com/topic/19776749/organize/entire'
+max_level = 3
+
+
+def get_first_level_topic_url(url):  # 广度优先遍历根话题下第一层话题
+    topic_level = 1  # 设置第一层话题的深度为1
+    html = new_new_zhihu_common.post(url)  # 知乎话题树的请求方式是POST
+    json_data = json.loads(html)  # 解析json网页内容
+    for item in json_data['msg'][1]:
+        topic_name = item[0][1]  # 话题名称
+        topic_id = item[0][2]  # 话题id
+        if topic_id != '19776751':  # 「未归类」话题，多是话题别名，不作考虑
+            topic_info = {
+                'topic_name': topic_name,
+                'topic_id': topic_id,
+                'topic_level': topic_level,
+                'parent_topic_name': json_data['msg'][0][1],
+                'parent_topic_id': json_data['msg'][0][2]
+            }
+            print(topic_info)
+            save_topic_list(topic_info)
+            yield start_url + '?child=&parent={}'.format(topic_id), topic_level + 1  # 生成第二层话题的地址和深度
+
+
+def get_topic_info(url, topic_level):  # 深度优先遍历
+    # time.sleep(1)
+    try:
+        if topic_level <= max_level:  # 判断话题爬虫深度
+            html = new_new_zhihu_common.post(url)
+            json_data = json.loads(html)
+            for item in json_data['msg'][1]:
+                topic_id = item[0][2]
+                topic_name = item[0][1]
+                if topic_name != '加载更多':  # 负责处理话题信息
+                    topic_info = {
+                        'topic_name': topic_name,
+                        'topic_id': topic_id,
+                        'topic_level': topic_level,
+                        'parent_topic_name': json_data['msg'][0][1],
+                        'parent_topic_id': json_data['msg'][0][2]
+                    }
+                    print(topic_info)
+                    save_topic_list(topic_info)
+                if item[0][1] == '加载更多':  # 负责翻页，实现广度遍历
+                    next_page_url = start_url + '?child={}&parent={}'.format(item[0][2], item[0][3])
+                    get_topic_info(next_page_url, topic_level)
+                else:
+                    if len(item) == 2:  # 判断是否有'显示子话题'，有则递归调用本身，实现深度遍历
+                        child_topic_url = start_url + '?child=&parent={}'.format(item[0][2])
+                        child_topic_level = topic_level + 1
+                        get_topic_info(child_topic_url, child_topic_level)
+    except:
+        print('Get_Topic_Info Error!')  # 若发生异常，则重新调用方法
+
+
+def save_topic_list(data):  # 保存话题信息到MongoDB数据库，方便其他爬虫调用
+    if mydb['zhihu_topic_list'].update({'topic_id': data['topic_id']}, {'$set': data}, True):  # 判断重复，若话题id重复，则不保存
+        print('Topic:', data['topic_name'], 'Has Been Saved Successfully...')
+    else:
+        print('Topic:', data['topic_name'], 'Saved Unsuccessfully...')
+
+
+def main():
+    for url, topic_level in get_first_level_topic_url(start_url):
+        get_topic_info(url, topic_level)
+
+
+if __name__ == '__main__':
+    main()
---
+++ zhihu_question_crawler.py
@@ -0,0 +1,81 @@
+import json
+import new_zhihu_common
+from new_zhihu_common import mydb
+
+topic_begin_url = 'https://www.zhihu.com/api/v4/topics/'
+
+question_level = 1
+limit = 5
+max_offset = 20  # 最大为1000
+
+
+def get_zhihu_topic_id():
+    for item in mydb['zhihu_topic_list'].find():
+        if item['topic_level'] == question_level:
+            yield item['topic_id'], item['topic_name']
+
+
+def get_zhihu_question(url, topic_id, topic_name):
+    html = new_zhihu_common.get(url)
+    json_data = json.loads(html)
+    try:
+        for item in json_data['data']:
+            if item['target'].get('question'):
+                question_title = item['target']['question']['title']
+                question_id = item['target']['question']['id']
+                zhihu_question_info = {
+                    'type': item['target']['question']['type'],
+                    'question_title': question_title,
+                    'question_id': question_id,
+                    'topic_name': topic_name,
+                    'topic_id': topic_id
+                }
+                print(zhihu_question_info)
+            elif item['target'].get('column'):
+                column_title = item['target']['column']['title']
+                column_id = item['target']['column']['id']
+                author_name = item['target']['column']['author']['name']
+                author_url_token = item['target']['column']['author']['url_token']
+                column_info = {
+                    'type': item['target']['column']['type'],
+                    'column_title': column_title,
+                    'column_id': column_id,
+                    'topic_name': topic_name,
+                    'topic_id': topic_id,
+                }
+                print(column_info)
+                user_info = {
+                    'type':item['target']['column']['author']['type'],
+                    'author_name': author_name,
+                    'author_url_token': author_url_token
+                }
+                # print(user_info)
+
+    except:
+        pass
+
+
+def get_zhihu_question_list():
+    for topic_id, topic_name in get_zhihu_topic_id():
+        for offset in range(0, max_offset, limit):
+            url = topic_begin_url + '{}/feeds/essence?limit={}&offset={}'.format(topic_id, limit, offset)
+            get_zhihu_question(url, topic_id, topic_name)
+
+
+def save_zhihu_question_list(data):
+    try:
+        if mydb['zhihu_question_list'].update({'question_id': data['question_id']}, {'$set': data},
+                                              True):  # 保存到MongoDB数据库，重复的则不保存
+            print(data['question_title'], 'Saving to MongoDB...')
+        else:
+            print(data['question_title'], 'Saving to MongoDB Failed')
+    except:
+        pass
+
+
+def main():
+    get_zhihu_question_list()
+
+
+if __name__ == '__main__':
+    main()
